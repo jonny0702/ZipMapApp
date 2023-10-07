dea6bb41d3bedeb1075fc49ccdc1d33bd18c0e83
jonny0702 <jonathanvergararuiz@gmail.com>
2023-10-07 18:41:37 -0500

    PROGRESS: Working in google maps SDK android

--- app/src/main/java/com/example/zipmap/MainActivity.kt
+++ app/src/main/java/com/example/zipmap/MainActivity.kt
@@ -4,13 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import androidx.fragment.app.add
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.commit{
-            add<InputZipCode>(R.id.fragment_container)
-        }
+        mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+
+            val location1 = LatLng(9.046973215050151, -79.48422789454311)
+            googleMap.addMarker(MarkerOptions().position(location1).title("my location!"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1, 10f))
+        })
     }
 }
