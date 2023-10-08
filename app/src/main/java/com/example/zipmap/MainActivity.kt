e183faa90e5050abb69b7a6eb9578318a6d31147
jonny0702 <jonathanvergararuiz@gmail.com>
2023-10-07 19:56:43 -0500

    FEATURE: Geolocation integration Api

--- app/src/main/java/com/example/zipmap/MainActivity.kt
+++ app/src/main/java/com/example/zipmap/MainActivity.kt
@@ -1,32 +1,76 @@
 package com.example.zipmap
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.commit
 import androidx.fragment.app.add
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapFragment
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
+
+    companion object{
+        private const val LOCATION_REQUEST_CODE = 100
+    }
 
     lateinit var mapFragment: SupportMapFragment
-    lateinit var googleMap: GoogleMap
+    lateinit var mMap: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
-        mapFragment.getMapAsync(OnMapReadyCallback {
-            googleMap = it
+        mapFragment.getMapAsync(this)
 
-            val location1 = LatLng(9.046973215050151, -79.48422789454311)
-            googleMap.addMarker(MarkerOptions().position(location1).title("my location!"))
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1, 10f))
-        })
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
+    override fun onMapReady(googleMap: GoogleMap){
+        mMap = googleMap
+
+       mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setOnMarkerClickListener(this)
+        setUpMap()
+    }
+
+    private fun setUpMap(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_REQUEST_CODE)
+            return
+        }
+        mMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) {location ->
+            if(location != null){
+                lastLocation = location
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLong)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 20f))
+            }
+        }
+    }
+    private fun placeMarkerOnMap(currentLatLong: LatLng){
+        val markerOptions = MarkerOptions().position(currentLatLong)
+        markerOptions.title("$currentLatLong")
+        mMap.addMarker(markerOptions)
+    }
+
+    override fun onMarkerClick(p0: Marker) = true
+
 }
