8177d7502666f47f099e813a6d1c6be284c3b5a7
Jose Marquinez <josemarquinez28@gmail.com>
2023-10-08 13:48:43 -0500

    feat: marker with current location

--- app/src/main/java/com/example/zipmap/MainActivity.kt
+++ app/src/main/java/com/example/zipmap/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.zipmap
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -13,20 +15,16 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    companion object {
-        private const val LOCATION_REQUEST_CODE = 100
-    }
-
+    private var locationPermissionGranted = false
+    private var lastKnownLocation: Location? = null
+    private val defaultLocation = LatLng(9.04, -79.48)
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var mMap: GoogleMap
-
-    //    private lateinit var lastLocation: Location
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,56 +34,118 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-//        mMap.uiSettings.isZoomControlsEnabled = false
-//        mMap.setOnMarkerClickListener(this)
-//        setUpMap()
-
-        val panama = LatLng(9.04, -79.48)
-        val markerText: String = getString(R.string.marker_title)
-
-        mMap.addMarker(
-            MarkerOptions().position(panama).title(markerText)
-        )
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(panama))
+        updateLocationUI()
+        getDeviceLocation()
     }
 
-    private fun setUpMap() {
+    private fun getLocationPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_REQUEST_CODE
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
             )
             return
         }
-        mMap.isMyLocationEnabled = true
-//        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
-//            if (location != null) {
-//                lastLocation = location
-//                val currentLatLong = LatLng(location.latitude, location.longitude)
-//                placeMarkerOnMap(currentLatLong)
-//                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 20f))
-//            }
-//        }
+
+        locationPermissionGranted = true
     }
-    
 
-//    private fun placeMarkerOnMap(currentLatLong: LatLng) {
-//        val markerOptions = MarkerOptions().position(currentLatLong)
-//        markerOptions.title("$currentLatLong")
-//        mMap.addMarker(markerOptions)
-//    }
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        if (mMap == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap.isMyLocationEnabled = true
+                mMap.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                mMap.isMyLocationEnabled = false
+                mMap.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
 
-//    override fun onMarkerClick(p0: Marker) = true
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        lastKnownLocation = task.result
 
+                        if (lastKnownLocation != null) {
+                            var currentLocation = LatLng(
+                                lastKnownLocation!!.latitude,
+                                lastKnownLocation!!.longitude
+                            )
+
+                            mMap?.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    currentLocation, DEFAULT_ZOOM.toFloat()
+                                )
+                            )
+
+                            val markerText = getString(R.string.marker_title)
+
+                            mMap.addMarker(
+                                MarkerOptions().position((currentLocation))
+                                    .title(markerText)
+                            )
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        mMap?.moveCamera(
+                            CameraUpdateFactory
+                                .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat())
+                        )
+                        mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    locationPermissionGranted = true
+                }
+            }
+
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+
+        updateLocationUI()
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    }
 }
