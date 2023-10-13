33a6d59b266f8349d796ffae92515801faeaff5c
jonny0702 <jonathanvergararuiz@gmail.com>
2023-10-12 21:22:15 -0500

    ✨ FEATURE: lottiefile splash screen

---
+++ app/src/main/java/com/example/zipmap/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.zipmap
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
